52be42b7d154d78ab7b45aba56849692d44a3dc7
MKARASKI <MKARASKI@CE19934.corp.capgemini.com>
2019-01-21 21:10:00 +0100

    With error

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/dao/SpecialDao.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/dao/SpecialDao.java
@@ -1,7 +1,9 @@
 package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api.dao;
 
+import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
 import io.oasp.gastronomy.restaurant.general.dataaccess.api.dao.ApplicationRevisionedDao;
 import io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api.SpecialEntity;
+import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SpecialSearchCriteriaTo;
 import io.oasp.module.jpa.dataaccess.api.MasterDataDao;
 
 /**
@@ -10,4 +12,19 @@ import io.oasp.module.jpa.dataaccess.api.MasterDataDao;
  */
 public interface SpecialDao extends ApplicationRevisionedDao<SpecialEntity>, MasterDataDao<SpecialEntity> {
 
+  /**
+   * Find active special offers for given date of checking.
+   *
+   * @param criteria with filled date of checking
+   * @return all found active special offers
+   */
+  List<SpecialEntity> findActiveSpecials(SpecialSearchCriteriaTo criteria);
+
+  /**
+   * Find active special offer for given date of checking and given offer number with best (smallest) price.
+   *
+   * @param criteria with filled date of checking, offer number
+   * @return best found special price
+   */
+  Money findBestActiveSpecial(SpecialSearchCriteriaTo criteria);
 }
