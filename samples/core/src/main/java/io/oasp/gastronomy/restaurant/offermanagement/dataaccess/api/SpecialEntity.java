9c2ed79dbbadbe833a47c63155889e0e00b8bc52
MKARASKI <MKARASKI@CE19934.corp.capgemini.com>
2019-01-21 15:55:02 +0100

    Special offer created

---
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
@@ -0,0 +1,96 @@
+package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
+import io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity;
+
+/**
+ * @author MKARASKI
+ *
+ */
+@Entity
+@Table(name = "Special")
+public class SpecialEntity extends ApplicationPersistenceEntity {
+
+  /**
+   * @return name
+   */
+  @Column(unique = true)
+  public String getName() {
+
+    return this.name;
+  }
+
+  /**
+   * @param name new value of {@link #getname}.
+   */
+  public void setName(String name) {
+
+    this.name = name;
+  }
+
+  /**
+   * @return offer
+   */
+  @ManyToOne
+  @JoinColumn(name = "offer_id")
+  public OfferEntity getOffer() {
+
+    return this.offer;
+  }
+
+  /**
+   * @param offer new value of {@link #getoffer}.
+   */
+  public void setOffer(OfferEntity offer) {
+
+    this.offer = offer;
+  }
+
+  /**
+   * @return activePeriod
+   */
+  @Embedded
+  public WeeklyPeriodEmbeddable getActivePeriod() {
+
+    return this.activePeriod;
+  }
+
+  /**
+   * @param activePeriod new value of {@link #getactivePeriod}.
+   */
+  public void setActivePeriod(WeeklyPeriodEmbeddable activePeriod) {
+
+    this.activePeriod = activePeriod;
+  }
+
+  /**
+   * @return specialPrice
+   */
+  public Money getSpecialPrice() {
+
+    return this.specialPrice;
+  }
+
+  /**
+   * @param specialPrice new value of {@link #getspecialPrice}.
+   */
+  public void setSpecialPrice(Money specialPrice) {
+
+    this.specialPrice = specialPrice;
+  }
+
+  private String name;
+
+  private OfferEntity offer;
+
+  private WeeklyPeriodEmbeddable activePeriod;
+
+  private Money specialPrice;
+}
